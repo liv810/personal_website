a1999b1204cecf4efb286fc76d66b384951bf96b
Olivia Angiuli <oangiuli@Olivias-Air.lan>
2018-08-22 23:06:22 -0700

    Convert website into Jekyll

---
+++ new website/_site/js/displaycard.js
@@ -0,0 +1,10 @@
+// window.onload = (e) => {
+//    $("#displaycard").mmenu({
+//          // Options
+//       });
+//       var API = $("#displaycard").data( "mmenu" );
+      
+//       $("#my-button").click(function() {
+//          API.open();
+//       });
+// };
